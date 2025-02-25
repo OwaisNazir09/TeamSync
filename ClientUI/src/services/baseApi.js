159b8aed78132a50d63b6afbe63d4a47f2745430
OwaisNazir <64866359+OwaisNazir09@users.noreply.github.com>
2025-02-25 09:50:47 +0530

    Update baseApi.js

--- ClientUI/src/services/baseApi.js
+++ ClientUI/src/services/baseApi.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const baseApi = createApi({
     reducerPath: 'api',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8523/' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://teamsyncbackend.onrender.com/' }),
     endpoints: () => ({}),
 });
 
